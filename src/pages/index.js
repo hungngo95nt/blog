aab45a3e60d7c50370b87ebbb235a4d8f83d35eb
hungngo95nt <hungngo95nt@gmail.com>
2020-05-18 16:54:11 +0700

    edit homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ class IndexPage extends React.Component {
             👋
           </span>
         </h1>
-        <p>Welcome to Hùng Ngô website. You are on your home page.</p>
+        <p>Welcome to Hùng Ngô website.</p>
         <p>
           This is my blog develop using Gatsby and Netlify CMS.
         </p>
