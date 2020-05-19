bf3515f833bc1335a2a3458b7f4af6121fe46806
hungngo95nt <hungngo95nt@gmail.com>
2020-05-19 18:23:32 +0700

    edit profile picture blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `hungngo95nt Blog`,
+    author: `Hùng Ngô`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `timngo95nt`,
     },
   },
   plugins: [
